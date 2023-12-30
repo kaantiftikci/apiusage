2102bad18ef92266c70396da40f91973a686ab38
Kaan <56001626+kaantiftikci@users.noreply.github.com>
2023-12-30 19:19:12 +0300

    Update deeplapi.py

--- deeplapi.py
+++ deeplapi.py
@@ -4,9 +4,9 @@ import pandas as pd
 import requests
 
 
-deepl_api_key = 'cae20839-82b8-4ab6-96b7-df5084a2b4bf:fx'
+deepl_api_key = 'DEEPL API KEY'
 
-df = pd.read_excel('/content/3501-5801.xlsx')
+df = pd.read_excel('file patch')
 
 
 def translate_text(text, target_lang='EN'):
@@ -24,5 +24,5 @@ def translate_text(text, target_lang='EN'):
 
 df['Translated_Review'] = df['review'].apply(lambda x: translate_text(x))
 
-df.to_excel('translated_reviews_3501-5801.xlsx', index=False)
+df.to_excel('file_name.xlsx', index=False)
 
